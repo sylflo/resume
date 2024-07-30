7360ed97724721f7bea645b2a3e1861117d69f76
sylvain.chateau <sylvain.chateau@outscale.com>
2024-07-31 01:30:06 +0200

    using port 465 for SSL instead of port 587

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel
 
 
 MAIL_SERVER = os.getenv("MAIL_SERVER")
-MAIL_PORT = 587
+MAIL_PORT = 465
 MAIL_USERNAME = os.getenv("MAIL_USERNAME")
 MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
 
